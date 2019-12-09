e18fbc3e21c1622da192cb00dddde30e701a18c5
moslixiansen <moslixiansen@163.com>
2019-12-09 18:11:30 +0800

    feat(add tree): add the directory of tree

---
+++ tree/binarySearchTree.js
@@ -0,0 +1,18 @@
+/**
+ * 二叉查找树: 任意一个节点的值大于或等于其左节点小于或等于其右节点
+ */
+class BST {
+  constructor (node = null) {
+    this.root = node
+  }
+
+  insert (data) {
+    if (this.root === null) {
+      return this.root.data = data
+    } else {
+      let parent = this.root
+      while (true) {
+      }
+    }
+  }
+}
---
+++ tree/node.js
@@ -0,0 +1,13 @@
+class Node {
+  constructor () {
+    this.data = null
+    this.left = null
+    this.right = null
+  }
+
+  show () {
+    return this.data
+  }
+}
+
+export default Node
